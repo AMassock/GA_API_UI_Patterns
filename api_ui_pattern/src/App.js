2ffc065e42097b9609dc70c74ae5e2fcd9a36db6
amassock <ajmassock@gmail.com>
2023-02-13 22:14:01 -0600

    fix router dom issue, start modal functionality

--- api_ui_pattern/src/App.js
+++ api_ui_pattern/src/App.js
@@ -1,15 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Modal from './Modal';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const url = 'https://pokeapi.co/api/v2/pokemon?limit=250'
   const [openModal, setOpenModal] = useState(false);
+  const [pokeList, setPokeList] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setPokeList(data.data))
+      .catch((err) => console.log('oop something went wrong', err))
+    },[])
+
+    console.log(pokeList);
 
   return (
     <div className="App">
-      <button>Modal</button>
-      <Modal open={openModal} />
+      <p>test</p>
+      <button onClick={() => setOpenModal(true)}>Open Modal</button>
+      <Modal open={openModal} onClose={() => setOpenModal(false)}/>
     </div>
   );
 }
--- api_ui_pattern/src/Modal.js
+++ api_ui_pattern/src/Modal.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-export default function Modal({open}) {
+export default function Modal({open, onClose}) {
     if(!open) return null
 
     return(
         <div>
-            <img src="https://www.akc.org/wp-content/uploads/2017/11/Affenpinscher-running-outdoors.jpg"></img>
+            <h1>test</h1>
+            <p onClick={onClose}className="closeBtn">X</p>
+            <h1 className="pokeName">Pokemon Name</h1>
+            <p className="pokeDesc">Pokemon Description</p>
         </div>
     )
 }
