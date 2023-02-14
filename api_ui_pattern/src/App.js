0b00cbd8d11ae909d50f6a624460ccd5d9d2caec
amassock <ajmassock@gmail.com>
2023-02-14 13:16:45 -0600

    pull in api data

--- api_ui_pattern/src/App.js
+++ api_ui_pattern/src/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import Modal from './Modal';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const url = 'https://pokeapi.co/api/v2/pokemon?limit=250'
+  const URL = 'https://pokeapi.co/api/v2/pokemon?limit=250';
+
   const [openModal, setOpenModal] = useState(false);
   const [pokeList, setPokeList] = useState([]);
 
   useEffect(() => {
-    fetch(url)
+    fetch(URL)
       .then((res) => res.json())
-      .then((data) => setPokeList(data.data))
+      .then((data) => setPokeList(data.results))
       .catch((err) => console.log('oop something went wrong', err))
-    },[])
+    }, [])
 
     console.log(pokeList);
 
@@ -21,7 +21,7 @@ function App() {
     <div className="App">
       <p>test</p>
       <button onClick={() => setOpenModal(true)}>Open Modal</button>
-      <Modal open={openModal} onClose={() => setOpenModal(false)}/>
+      <Modal open={openModal} onClose={() => setOpenModal(false)} />
     </div>
   );
 }
